e3b5e9c88d6371c722dc7d259fd75e5822615ba5
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2020-09-29 01:50:29 -0500

    Update

--- app/src/main/java/com/example/proyecto_2do/MainActivity.java
+++ app/src/main/java/com/example/proyecto_2do/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.proyecto_2do;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
