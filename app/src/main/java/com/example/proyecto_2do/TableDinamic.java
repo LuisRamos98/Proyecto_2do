9f7a7068f045937c91526202e31947abd33ecb40
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2020-10-10 21:44:38 -0500

    problema login solve

--- app/src/main/java/com/example/proyecto_2do/TableDinamic.java
+++ app/src/main/java/com/example/proyecto_2do/TableDinamic.java
@@ -63,6 +63,7 @@ public class TableDinamic {
         tableLayout.addView(tableRow);
     }
 
+
     //CREA LA TABLE EN SI MISMA
     private void createDataTable(ArrayList<String[]>data){
         tableLayout.removeAllViews();
